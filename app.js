dfcea0c70fb57019bd8595f73951d9a313189de8
megastar98 <obikoya11@gmail.com>
2017-09-20 17:16:42 +0100

    added speeddtest middleware

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
 #!/usr/bin/env node
-console.log("Hello",process.argv[2]);
+// console.log("Hello",process.argv[2]);
+var speedTest    =  require('speedtest-net');
+var ProgressBar  =  require('progress');
+var chalk        =  require("chalk");
+
+var test = speedTest({maxTime:5000});
+
+var bar,pingTime;
+test.on('data',data=>{
+    console.dir(data);
+});
+test.on('error',err =>{
+    console.error(err);
+});
+
